7a7fc1fa5132c51f1bd42144f9b53e52634726e4
Diana Pham <dianaphamis@gmail.com>
2018-12-04 18:47:50 -0800

    - API call works! :D

---
+++ script.js
@@ -0,0 +1,11 @@
+var requestURL = "http://api.openweathermap.org/data/2.5/weather?q=Seattle&APPID=04d3244ad67b8088a3ddf5746a5ac0de";
+var request = new XMLHttpRequest();
+
+request.open("GET", requestURL);
+request.responseType = 'json';
+request.send();
+
+request.onload = function(){
+	var weatherData = request.response;
+	console.log(weatherData);
+}
